9a1444f7615e7dc7591e95987897cfc5050f5614
Robert Nastasa <nrarobert@gmail.com>
2016-02-08 17:32:19 +0200

    .

--- scripts/script.js
+++ scripts/script.js
@@ -10,16 +10,3 @@ $('nav ul li a').click(function() {
         $child.addClass('active');
     }
 });
-
-$(function() {
-  $('a[href^="#"]:not([href="#"])').click(function() {
-     var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-    }
-  });
-});
